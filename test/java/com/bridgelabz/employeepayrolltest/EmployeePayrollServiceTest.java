55890d8ad2ddd967ecfa651987cf258d26bdad1c
Shubham Rana <ranashubham7296@gmail.com>
2020-11-06 22:57:12 +0530

    Added: EmployeePayrollTest to print data added to the file.

---
+++ test/java/com/bridgelabz/employeepayrolltest/EmployeePayrollServiceTest.java
@@ -0,0 +1,31 @@
+package com.bridgelabz.employeepayrolltest;
+
+import java.util.Arrays;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.bridgelabz.employepayrollservice.EmployeePayrollData;
+import com.bridgelabz.employepayrollservice.EmployeePayrollService;
+
+import static com.bridgelabz.employepayrollservice.EmployeePayrollService.IOService.FILE_IO;;
+
+public class EmployeePayrollServiceTest 
+{
+	@Test
+	public void given3Employees_whenWrittenToFile_shouldMatchEmployeeEntries()
+	{
+		EmployeePayrollData[] arraysOfEmps = {
+				new EmployeePayrollData(1, "Shubham", 100000.0),
+				new EmployeePayrollData(2, "Rajat", 500000.0),
+				new EmployeePayrollData(3, "Mayank", 700000.0)
+		};
+		
+		EmployeePayrollService employeePayrollService = new EmployeePayrollService(Arrays.asList(arraysOfEmps));
+		employeePayrollService.writeEmployeePayrollData(FILE_IO);
+		employeePayrollService.print(FILE_IO);
+		
+		long entries = employeePayrollService.countEntries(FILE_IO);
+		Assert.assertEquals(3, entries);
+	}
+}
